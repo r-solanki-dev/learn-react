df343ccf3d5af1e708dce33444a5d5482760217c
Riya Solanki <riyahsolanki@gmail.com>
2025-12-02 21:55:31 -0600

    15. Customer Components Challenge Part 2

--- 01. Static pages/15. Custom Components Challenge Part 2/index.jsx
+++ 01. Static pages/15. Custom Components Challenge Part 2/index.jsx
@@ -20,12 +20,23 @@ Part 2:
 
 function Page() {
     return (
-        <ol>
-            <li>React is a popular library, so I will be able to
-            fit in with all the coolest devs out there! 😎</li>
-            <li>I am more likely to get a job as a front end developer
-            if I know React</li>
-        </ol>
+        <>
+            <header>
+                <img src="react-logo.png" alt="react logo" width="40px"/>
+            </header>
+            <main>
+                <h1>Reasons I am excited to learn React</h1>
+                <ol>
+                    <li>React is a popular library, so I will be able to
+                    fit in with all the coolest devs out there! 😎</li>
+                    <li>I am more likely to get a job as a front end developer
+                    if I know React</li>
+                </ol>
+            </main>
+            <footer>
+                © 2025 Solanki development. All rights reserved.
+            </footer>
+        </>
     )
 }
 
