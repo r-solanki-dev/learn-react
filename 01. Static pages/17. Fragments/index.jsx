f3f89ed9206b3adea388e9c8e9cc5331304fa772
Riya Solanki <riyahsolanki@gmail.com>
2025-12-04 21:42:36 -0600

    17. Fragments

--- 01. Static pages/17. Fragments/index.jsx
+++ 01. Static pages/17. Fragments/index.jsx
@@ -4,20 +4,38 @@ const root = createRoot(document.getElementById("root"))
 function Page() {
     return (
         <div>
-            <header>
-                <img src="react-logo.png" width="40px" alt="React logo" />
-            </header>
-            <main>
-                <h1>Reason I am excited to learn React</h1>
-                <ol>
-                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                    <li>I am more likely to get a job as a front end developer if I know React</li>
-                </ol>
-            </main>
-            <footer>
-                <small>© 2024 Ziroll development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <MainComponent />
+            <Footer />
         </div>
+    )
+}
+
+function Header() {
+    return(
+        <header>
+            <img src="react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+function MainComponent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
     )
 }
 
