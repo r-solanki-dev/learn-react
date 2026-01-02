339d225a0ee6c978b6e6bb700aa31cc06bfc37f3
Riya Solanki <riyahsolanki@gmail.com>
2026-01-01 23:13:42 -0600

    main content section

--- 01. Static pages/24. ReactFacts Project - Main Content Section/components/Main.jsx
+++ 01. Static pages/24. ReactFacts Project - Main Content Section/components/Main.jsx
@@ -8,6 +8,17 @@
 
 export default function Main() {
     return (
-        <h1>Main component here</h1>
+        <section>
+            <div>
+                <span>Fun facts about React</span>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 200K stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        </section>
     )
 }
