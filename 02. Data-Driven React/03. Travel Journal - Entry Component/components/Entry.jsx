f5c40f5bf39d8e754dd9a9c645953c4832c11a91
Riya Solanki <riyahsolanki@gmail.com>
2026-01-10 20:33:04 -0600

    starting entry component

--- 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
+++ 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
@@ -13,3 +13,34 @@
  *   currently are, so don't worry about the fact that you're hard-coding all
  *   this data into the component.
  */
+
+export default function Entry () {
+    return (
+        <div className="entry">
+            <img 
+                className=""
+                src="../images/japan.png"
+            />
+            <div>
+                <span className="details">
+                    <img 
+                        className="marker"
+                        src="../images/marker.png"
+                    />
+                    <p className="country">Japan</p>
+                    <a 
+                        className="maps-link"
+                        href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+                    >
+                        View on Google Maps
+                    </a>
+                </span>
+                <h1 className="location">Mount Fuji</h1>
+                <p className="dates">12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className="description">
+                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                </p>
+            </div>
+        </div>
+    )
+}
