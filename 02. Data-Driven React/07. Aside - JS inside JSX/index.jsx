28509663a28e033193e4983ee6460a0888b3dce9
Riya Solanki <riyahsolanki@gmail.com>
2026-01-11 18:47:44 -0600

    jsx

--- 02. Data-Driven React/07. Aside - JS inside JSX/index.jsx
+++ 02. Data-Driven React/07. Aside - JS inside JSX/index.jsx
@@ -1,15 +1,21 @@
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  const firstName = "Joe"
-  const lastName = "Schmoe"
-  
-  /**
-   * Challenge: finish off the h1 below so it says "Hello Joe Schmoe"
-   */
+  const hours = new Date().getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else if (hours < 21) {
+    timeOfDay = "evening"
+  } else {
+    timeOfDay = "night"
+  }
   
   return (
-    <h1>Hello {firstName} + " " + lastName</h1>
+    <h1>Good {timeOfDay}</h1>
   )
 }
 
