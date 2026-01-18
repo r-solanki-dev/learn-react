697ea3bcae5b4c9ad4a1e45e1b79a49817f13484
Riya Solanki <riyahsolanki@gmail.com>
2026-01-17 20:03:08 -0600

    react arrays

--- 02. Data-Driven React/18. React can render arrays/App.jsx
+++ 02. Data-Driven React/18. React can render arrays/App.jsx
@@ -7,7 +7,13 @@ export default function App() {
      * JSX elements by surrounding each ninja turtle with an <h2> element
      */
     
-    const ninjaTurtles = ["Donatello", "Michaelangelo", "Rafael", "Leonardo"]
+    const ninjaTurtles = [
+        <h2>Donatello</h2>, 
+        <h2>Michaelangelo</h2>, 
+        <h2>Rafael</h2>, 
+        <h2>Leonardo</h2>
+    ]
+
     return (
         <main>
             {ninjaTurtles}
