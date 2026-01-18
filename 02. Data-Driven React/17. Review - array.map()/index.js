13ab3b78c820d11443e9e1a64c2daddc2e6c4865
Riya Solanki <riyahsolanki@gmail.com>
2026-01-17 19:40:46 -0600

    array.map()

--- 02. Data-Driven React/17. Review - array.map()/index.js
+++ 02. Data-Driven React/17. Review - array.map()/index.js
@@ -4,10 +4,8 @@ Given an array of numbers, return an array of each number, squared
 */
 const nums = [1, 2, 3, 4, 5]
 // -->       [1, 4, 9, 16, 25]
-// Your code here
-
-
-
+const squares = nums.map((num) => { return num ** 2 })
+console.log(squares)
 
 /*
 Challenge 2:
@@ -17,9 +15,8 @@ the first letter of each string is capitalized
 
 const names = ["alice", "bob", "charlie", "danielle"]
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
-// Your code here
-
-
+const capitalized = names.map((name) => {return name.charAt(0).toUpperCase() + name.substring(1)})
+console.log(capitalized)
 
 
 /*
@@ -33,6 +30,8 @@ return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 
 const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
 // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
-// Your code here
+const htmlPokemon = pokemon.map((name) => { 
+    return (`<p>${name}</p>`)
+})
 
-
+console.log(htmlPokemon)
