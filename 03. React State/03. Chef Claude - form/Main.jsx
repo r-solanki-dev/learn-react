99cb64f3a34d7fe485634241d641fd1719251157
Riya Solanki <riyahsolanki@gmail.com>
2026-01-18 21:56:51 -0600

    some styling

---
+++ 03. React State/03. Chef Claude - form/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main () {
+    return (
+        <form>
+            <input 
+                placeholder="e.g. oregano"
+            ></input>
+            <button>+ Add Ingredient</button>
+        </form>
+    )
+}
