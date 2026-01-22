df492e85842535c6eca2fab78ea85fdcccfeee3c
Riya Solanki <riyahsolanki@gmail.com>
2026-01-21 20:29:03 -0600

    form submission

--- 03. React State/23. Form submission/index.jsx
+++ 03. React State/23. Form submission/index.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    console.log(formData.get("text"))
+    event.currentTarget.reset()
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="text">Email:</label>
+        <input id="text" type="text" name="text" placeholder="joe@schmoe.com" />
         <br />
         
         <label htmlFor="password">Password:</label>
-        <input id="password" type="password" name="password" />
+        <input id="password" type="password" name="password"/>
         <br />
         
         <button>Submit</button>
