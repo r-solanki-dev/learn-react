454369087691b39b4ab0be50f16cc97955d20762
Riya Solanki <riyahsolanki@gmail.com>
2026-01-21 20:03:36 -0600

    basic form

--- 03. React State/22. Form basics/index.jsx
+++ 03. React State/22. Form basics/index.jsx
@@ -13,9 +13,10 @@ function App() {
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
         <br />
-        
-        
-        
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" name="password" />
+        <br />
+        <button type="submit">Submit</button>
       </form>
     </section>
   )
