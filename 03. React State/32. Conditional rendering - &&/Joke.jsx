44be6fc4dfd23b4e29c995a9321115d41cb77dac
Riya Solanki <riyahsolanki@gmail.com>
2026-01-25 15:20:22 -0600

    &&

--- 03. React State/32. Conditional rendering - &&/Joke.jsx
+++ 03. React State/32. Conditional rendering - &&/Joke.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react"
 export default function Joke(props) {
     /**
      * Challenge:
      * - Create state `isShown` (boolean, default to `false`)
      * - Add a button that toggles the value back and forth
      */
+
+    const [isShown, setIsShown] = useState(false)
+
+    function toggleShown() {
+        setIsShown((prevIsShown) => !prevIsShown)
+        console.log(isShown)
+    }
     
-    console.log(isShown)
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            <button onClick={toggleShown}>{isShown ? "Hide" : "Show me"} the punchline</button>
+            {isShown && <p>{props.punchline}</p>}
             <hr />
         </div>
     )
