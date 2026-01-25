afb57d97f67d84ad302d308938ba9169eab03195
Riya Solanki <riyahsolanki@gmail.com>
2026-01-25 15:27:01 -0600

    && practice

--- 03. React State/34. Conditional rendering practice - &&/App.jsx
+++ 03. React State/34. Conditional rendering practice - &&/App.jsx
@@ -1,16 +1,20 @@
 import React from "react"
 
 export default function App() {
-    const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"])
-    
-    /**
-     * Challenge:
-     * Only display the <h1> below if there are unread messages
-     */
+    const [unreadMessages, setUnreadMessages] = React.useState([])
+
+    const countUnreadMessages = unreadMessages.length
     
     return (
         <div>
-            <h1>You have _ unread messages!</h1>
+            {
+                countUnreadMessages > 0 && 
+                <h1>You have {countUnreadMessages} unread messages!</h1>
+            }
+            {
+                countUnreadMessages == 0 &&
+                <h1>You have no unread messages.</h1>
+            }
         </div>
     )
 }
