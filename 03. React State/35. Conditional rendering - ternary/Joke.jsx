128e0abc0ea40faa175f10090aa3ee8eedbc331a
Riya Solanki <riyahsolanki@gmail.com>
2026-01-25 15:34:55 -0600

    ternary

--- 03. React State/35. Conditional rendering - ternary/Joke.jsx
+++ 03. React State/35. Conditional rendering - ternary/Joke.jsx
@@ -10,8 +10,8 @@ export default function Joke(props) {
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            {isShown && <p>{props.punchline}</p>}
-            <button onClick={toggleShown}>Show punchline</button>
+            {isShown ? <p>{props.punchline}</p> : null}
+            <button onClick={toggleShown}>{isShown ? "Hide" : "Show"} punchline</button>
             <hr />
         </div>
     )
