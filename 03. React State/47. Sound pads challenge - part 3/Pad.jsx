930a9d4d02e380b4772c97a70e782afe7bbef0b1
Riya Solanki <riyahsolanki@gmail.com>
2026-01-26 22:24:21 -0600

    part 3

--- 03. React State/47. Sound pads challenge - part 3/Pad.jsx
+++ 03. React State/47. Sound pads challenge - part 3/Pad.jsx
@@ -4,10 +4,13 @@ export default function Pad(props) {
      * Our buttons got turned off by default! Update the code
      * so if the button is "on", it has the className of "on".
      */
+
+    console.log(props.on)
     
     return (
         <button 
             style={{backgroundColor: props.color}}
+            className={ props.on ? "on" : undefined }
         ></button>
     )
 }
