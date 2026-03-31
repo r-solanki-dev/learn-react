e790e7a1b89c23537d58240b788f39673fcae1dd
Riya Solanki <riyahsolanki@gmail.com>
2026-03-31 00:07:00 -0500

    confetti

--- 06. Capstone Project 2 - Assembly Endgame/25. Assembly Endgame/App.jsx
+++ 06. Capstone Project 2 - Assembly Endgame/25. Assembly Endgame/App.jsx
@@ -140,6 +140,7 @@ export default function AssemblyEndgame() {
 
     return (
         <main>
+            {isGameWon && <Confetti />}
             <header>
                 <h1>Assembly: Endgame</h1>
                 <p>Guess the word within 8 attempts to keep the
